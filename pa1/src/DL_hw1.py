7d481d9d2135e49127a95cb510ff354cdbf76da7
Giacomo Grandi <jak.grandi@gmail.com>
2022-02-09 18:31:11 +0200

    first personal commit

--- pa1/src/DL_hw1.py
+++ pa1/src/DL_hw1.py
@@ -217,3 +217,5 @@ with torch.no_grad():
                  (' '.join(tweet['BODY'][:-1]), tweet['SENTIMENT'], gold_class, predicted))
 
     print('Test accuracy on development set: %.2f' % (100.0 * correct / len(data['development.gold'])))
+
+    # ciao
